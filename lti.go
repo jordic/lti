3e181dd11073692f72501ad405a094f3403edb3a
jordi <jordic@gmail.com>
2016-02-10 10:23:47 +0100

    API defined for validating requests

--- lti.go
+++ lti.go
@@ -6,40 +6,124 @@
 package lti
 
 import (
+	"crypto/rand"
+	"encoding/binary"
+	"net/http"
 	"net/url"
+	"strconv"
 	"strings"
+	"sync/atomic"
+	"time"
 
 	"github.com/jordic/lti/oauth"
 )
 
+const (
+	OAuthVersion = "1.0"
+	SigHMAC      = "HMAC-SHA1"
+)
+
 // Provider is an app, that can consume LTI messages
 type Provider struct {
-	Secret string
-	URL    string
-	values url.Values
+	Secret      string
+	URL         string
+	ConsumerKey string
+	Method      string
+	values      url.Values
+	r           *http.Request
+	Signer      oauth.OauthSigner
 }
 
-// Get a value from the producer
+// Default is a default provider
+func Default(secret string) *Provider {
+	sig := oauth.GetHMACSigner(secret, "")
+	return &Provider{
+		Secret: secret,
+		Method: "POST",
+		values: url.Values{},
+		Signer: sig,
+	}
+}
+
+// Get a value from the provider
 func (p *Provider) Get(k string) string {
 	return p.values.Get(k)
 }
 
-// IsValid returns if lti request is valid
-func (p *Provider) IsValid() bool {
-	return true
+// Params returns request params
+func (p *Provider) Params() url.Values {
+	return p.values
+}
+
+// Add a new param
+func (p *Provider) Add(k, v string) *Provider {
+	if p.values == nil {
+		p.values = url.Values{}
+	}
+	p.values.Set(k, v)
+	return p
+}
+
+// Empty checks if a key is defined (or has something)
+func (p *Provider) Empty(key string) bool {
+	if p.values == nil {
+		p.values = url.Values{}
+	}
+	return p.values.Get(key) == ""
+}
+
+// Sign a request, adding, required fields
+func (p *Provider) Sign() (string, error) {
+	if p.Empty("oauth_version") {
+		p.Add("oauth_version", OAuthVersion)
+	}
+	if p.Empty("oauth_timestamp") {
+		p.Add("oauth_timestamp", strconv.FormatInt(time.Now().Unix(), 10))
+	}
+	if p.Empty("oauth_nonce") {
+		p.Add("oauth_nonce", nonce())
+	}
+	p.Add("oauth_consumer_key", p.ConsumerKey)
+
+	signature, err := Sign(p.values, p.URL, p.Method, p.Signer)
+	if err == nil {
+		p.Add("oauth_signature", signature)
+	}
+	return signature, err
+}
+
+// IsValid returns if lti request is valid, currently only checks
+// if signature is correct
+func (p *Provider) IsValid(r *http.Request) (bool, error) {
+	if p.values == nil {
+		r.ParseForm()
+		p.values = r.Form
+	}
+	signature := r.Form.Get("oauth_signature")
+	sig, err := Sign(r.Form, r.URL.String(), r.Method, p.Signer)
+	if err != nil {
+		return false, err
+	}
+	if sig == signature {
+		return true, nil
+	}
+	return false, nil
+}
+
+// SetSigner oauth method
+func (p *Provider) SetSigner(s oauth.OauthSigner) {
+	p.Signer = s
 }
 
 // Sign a lti request using HMAC containing a u, url, a http method,
 // and a secret. ts is a tokenSecret field from the oauth spec,
 // that in this case must be empty.
-func Sign(form url.Values, u, method, secret, ts string) (string, error) {
-
+func Sign(form url.Values, u, method string, firm oauth.OauthSigner) (string, error) {
 	str, err := getBaseString(method, u, form)
 	if err != nil {
 		return "", err
 	}
-	hmac := oauth.GetHMACSigner(secret, ts)
-	sig, err := hmac.GetSignature(str)
+	sig, err := firm.GetSignature(str)
 	if err != nil {
 		return "", err
 	}
@@ -63,4 +147,17 @@ func getBaseString(m, u string, form url.Values) (string, error) {
 	// ugly patch for formatting string as expected.
 	str = strings.Replace(str, "%2B", "%2520", -1)
 	return str, nil
+}
+
+var nonceCounter uint64
+
+// nonce returns a unique string.
+func nonce() string {
+	n := atomic.AddUint64(&nonceCounter, 1)
+	if n == 1 {
+		binary.Read(rand.Reader, binary.BigEndian, &n)
+		n ^= uint64(time.Now().UnixNano())
+		atomic.CompareAndSwapUint64(&nonceCounter, 1, n)
+	}
+	return strconv.FormatUint(n, 16)
 }
--- lti_test.go
+++ lti_test.go
@@ -4,51 +4,37 @@ import (
 	"log"
 	"net/url"
 	"testing"
+
+	"github.com/jordic/lti/oauth"
 )
 
-func TestSignData2(t *testing.T) {
-	form := url.Values{}
-	form.Add("oauth_version", "1.0")
-	form.Add("oauth_timestamp", "145379781y9")
-	form.Add("oauth_nonce", "a6e69e5d34b9ff0b140c71942bc8615yy2")
-	form.Add("oauth_consumer_key", "speakApps")
-	form.Add("custom_aws_region", "ec2.eu-west-1.amazonaws.com")
-	form.Add("resource_link_id", "1086")
-	form.Add("resource_link_title", "Aws Amazon")
-	form.Add("resource_link_description", "Aws Amazon ")
-	form.Add("user_id", "2")
-	form.Add("custom_username", "admin")
-	form.Add("lis_person_sourcedid", "speakApps:admin")
-	form.Add("roles", "Instructor,urn:lti:sysrole:ims/lis/Administrator")
-	form.Add("context_id", "2")
-	form.Add("context_label", "SAC101")
-	form.Add("context_title", "SpeakApps Course Test")
-	form.Add("launch_presentation_locale", "ca")
-	form.Add("lis_person_nam_given", "Admin")
-	form.Add("lis_person_name_family", "SpeakApps")
-	form.Add("lis_person_name_full", "Admin SpeakApps")
-	form.Add("lis_person_contact_email_primary", "speakapps@uoc.edu")
-	form.Add("ext_lms", "moodle-2")
-	form.Add("tool_consumer_info_product_family_code", "moodle")
-	form.Add("tool_consumer_info_version", "2014051200.09")
-	form.Add("oauth_callback", "about:blank")
-	form.Add("ext_submit", "Press to submit")
-	form.Add("lti_version", "LTI-1p0")
-	form.Add("lti_message_type", "basic-lti-launch-request")
-	form.Add("tool_consumer_instance_guid", "moodle.speakapps.org")
-	form.Add("launch_presentation_return_url", "http://moodle.speakapps.org/mod/lti/return.php?course=2&amp;launch_container=4&amp;instanceid=1086&amp;sesskey=r3B3skhlho")
-	form.Add("oauth_signature_method", "HMAC-SHA1")
+func TestCreateRequest(t *testing.T) {
+	signer := oauth.GetHMACSigner("asdf", "")
 
-	log.Printf("Encode %s", form.Encode())
-
-	s, err := Sign(form, "http://54.246.81.79/gestioaws",
-		"post", "speakApps2011*", "")
-	if err != nil {
-		t.Errorf("Can't sign %s", err)
+	p := &Provider{
+		Secret:      "asdf",
+		URL:         "https://urltest.com",
+		ConsumerKey: "1234",
+		Method:      "post",
 	}
 
-	if s != "b4VwGe2fTJGiy5J7QqbK/neuRuo=" {
-		t.Errorf("Wrong signature %s", s)
+	p.Add("resource_link_id", "1086").
+		Add("context_id", "2").
+		Add("roles", "Instructor,Administrator").
+		Add("custom_username", "test").
+		SetSigner(signer)
+
+	sig, err := p.Sign()
+	if err != nil {
+		t.Errorf("Error signing %s", err)
+	}
+	if sig == "" {
+		t.Errorf("Request should be signed")
+	}
+
+	if p.Get("oauth_signature") != sig {
+		t.Errorf("Request should be signed, %s", p.Get("oauth_signature"))
+		log.Printf("%#v", p.values)
 	}
 
 }
@@ -66,12 +52,12 @@ func TestSign(t *testing.T) {
 	}
 
 	if len(res) != len(OT) {
-		t.Error("Wrong lengths %d, %d", len(res), len(OT))
+		t.Errorf("Wrong lengths %d, %d", len(res), len(OT))
 	}
 
 	for i := range res {
 		if res[i] != OT[i] {
-			t.Error("%s, %s", res[i:(i+15)], OT[i:(i+15)])
+			t.Errorf("%s, %s", res[i:(i+15)], OT[i:(i+15)])
 			break
 		}
 	}
@@ -80,16 +66,17 @@ func TestSign(t *testing.T) {
 		t.Errorf("%s\n%s", OT, res)
 	}
 
+	signer := oauth.GetHMACSigner("secret", "")
 	signed, err := Sign(vals,
 		"http://www.imsglobal.org/developers/LTI/test/v1p1/tool.php",
-		"post", "secret", "")
+		"post", signer)
 
 	if err != nil {
 		t.Errorf("Error sigining request %s", err)
 	}
 
 	if signed != "QWgJfKpJNDrpncgO9oXxJb8vHiE=" {
-		t.Error("Provided %s", signed)
+		t.Errorf("Provided %s", signed)
 	}
 
 }
